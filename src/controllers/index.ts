4f571d377aeea87218f3e7d5ce1817d0cf57972e
See Yihui <cuoguo@gmail.com>
2018-10-24 18:12:55 +0800

    Removed unused index controller

--- src/controllers/index.ts
+++
@@ -1,8 +0,0 @@
-import { Context } from 'koa';
-class Controller {
-    public static getHealth = (ctx: Context) => {
-        ctx.status = 200;
-        ctx.body = 'API endpoint is running';
-    }
-}
-export default Controller
