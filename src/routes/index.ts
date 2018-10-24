eb378d1b4bed342817f1c0200e2f491759f87bdb
See Yihui <cuoguo@gmail.com>
2018-10-24 18:13:03 +0800

    Add health endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import * as Router from 'koa-router';
 import userRouter from './user.routes';
-// import Controller from '../controllers';
 
 const router = new Router();
 
 router.prefix('/v0');
-// router.get('/health', Controller)
+router.get('/health', (ctx, next) => {
+  ctx.status = 200;
+  ctx.body = 'API endpoint is running'
+});
 
 // user specific routes. path defined as /v1/user/
 router.use('/user', userRouter.routes(), userRouter.allowedMethods())
