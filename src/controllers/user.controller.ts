ce3897a6fafc0ec1dd88eb0e7dd2f85ed0fffdf1
See Yihui <cuoguo@gmail.com>
2018-10-25 09:45:42 +0800

    Implement methods to get user by id and get all users

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,10 +1,20 @@
 import { Context } from 'koa';
+import { getUser, getUsers } from '../model/user.model';
 
 class UserController {
-  public static getUser = (ctx: Context) => {
-    const user = ctx.request.query.user
+  public static getUser = async (ctx: Context) => {
+    const user = await getUser(ctx.params.id)
+      .catch((error) => {
+        ctx.throw(500, error.message) // @TODO: this message is not being exposed to user
+      });
     ctx.status = 200;
     ctx.body = user;
+  }
+
+  public static getUsers = async (ctx: Context) => {
+    const users = await getUsers();
+    ctx.status = 200;
+    ctx.body = users;
   }
 }
 
--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -1,1 +1,45 @@
 // used when interacting with db
+import User from '../entity/user.entity';
+
+const mockUsers: User[] =  [
+    {
+        name: 'Apple',
+        id: '15'
+    },
+    {
+        name: 'Banana',
+        id: '20'
+    },
+    {
+        name: 'Chocolate',
+        id: '23'
+    },
+    {
+        name: 'Doughnut',
+        id: '24'
+    }
+];
+
+export const getUser = (id: string) : Promise<User> => {
+    return new Promise((resolve, reject) =>{
+        // let user: User;
+        const user = mockUsers.find((user) => {
+            return user.id === id;
+        })
+        setTimeout(() => {
+            if (user) {
+                resolve(user)
+            } else {
+                reject({ message: 'Error retrieving user' })
+            }
+        }, 800);
+    })
+}
+
+export const getUsers = () : Promise<User[]> => {
+    return new Promise((resolve, reject) =>{
+        setTimeout(() => {
+            resolve(mockUsers)
+        }, 800);
+    })
+}
